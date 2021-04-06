2047786b94beb523a20e6483f9a6a55bb3f1d183
Tang Tou <tyu4520@gmail.com>
2021-04-06 20:22:25 +0800

    add init

---
+++ db/migrate/20210406104110_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :category
+      t.string :contry
+      t.text :brief
+      t.integer :votes
+
+      t.timestamps
+    end
+  end
+end
